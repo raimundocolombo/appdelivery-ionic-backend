ae57cc9d39bf4784241ce93b2de91ac8dd3d76b4
Raimundo Junior <raimundo.colombo@gmail.com>
2017-11-05 00:04:53 -0200

    Projeto Criado

---
+++ src/main/java/br/com/bsbmob/appdelivery/AppDeliveryApplication.java
@@ -0,0 +1,12 @@
+package br.com.bsbmob.appdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDeliveryApplication.class, args);
+	}
+}
