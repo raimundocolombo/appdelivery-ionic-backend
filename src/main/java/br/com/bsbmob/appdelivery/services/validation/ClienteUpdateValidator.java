f9fb9081cb2dbf65fc865c2e76267a735b1b3569
Raimundo Junior <raimundo.colombo@gmail.com>
2017-11-14 00:44:45 -0200

    Validação customizada: Email não repetido na atualização de cliente.

---
+++ src/main/java/br/com/bsbmob/appdelivery/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,54 @@
+package br.com.bsbmob.appdelivery.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.bsbmob.appdelivery.domain.Cliente;
+import br.com.bsbmob.appdelivery.dto.ClienteDTO;
+import br.com.bsbmob.appdelivery.repositories.ClienteRepository;
+import br.com.bsbmob.appdelivery.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private HttpServletRequest httpRequest;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>)httpRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if (cliente != null && !cliente.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existe!"));
+		}
+		
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
