ff45d762f0b2c6dc0e75005866120062e8972938
Raimundo Junior <raimundo.colombo@gmail.com>
2017-11-17 23:05:31 -0200

    Implementando toString() de Pedido.

--- src/main/java/br/com/bsbmob/appdelivery/services/PedidoService.java
+++ src/main/java/br/com/bsbmob/appdelivery/services/PedidoService.java
@@ -9,6 +9,7 @@ import br.com.bsbmob.appdelivery.domain.ItemPedido;
 import br.com.bsbmob.appdelivery.domain.PagamentoComBoleto;
 import br.com.bsbmob.appdelivery.domain.Pedido;
 import br.com.bsbmob.appdelivery.domain.enums.EstadoPagamento;
+import br.com.bsbmob.appdelivery.repositories.ClienteRepository;
 import br.com.bsbmob.appdelivery.repositories.ItemPedidoRepository;
 import br.com.bsbmob.appdelivery.repositories.PagamentoRepository;
 import br.com.bsbmob.appdelivery.repositories.PedidoRepository;
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public Pedido find(Integer id) {
 		Pedido ped = repo.findOne(id);
 		if (ped == null) {
@@ -44,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -54,10 +59,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.save(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 
